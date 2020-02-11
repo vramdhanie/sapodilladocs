f32fc527c903bdaede317cac8bb65d4164f5f503
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-02-11 13:55:41 -0400

    Initialize the documentation  project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sapodilla Documentation
+
+This is the official documentation for Sapodilla Tech. In this site you will find the developer onboarding, coding standard and processes for developing code at Sapodilla.
+
+## Attribution
+
+This website is built using [Docusaurus 2](https://v2.docusaurus.io/), a modern static website generator.
---
+++ docusaurus.config.js
@@ -0,0 +1,90 @@
+module.exports = {
+  title: "Sapodilla Documentation",
+  tagline: "Documenting the process for our future selves",
+  url: "https://docs.sapodillatech.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "sapodilla", // Usually your GitHub org/user name.
+  projectName: "sapodilladocs", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Sapodilla",
+      logo: {
+        alt: "Sapodilla Logo",
+        src: "img/logo.svg"
+      },
+      links: [
+        { to: "docs/doc1", label: "Docs", position: "left" },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/facebook/docusaurus",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Style Guide",
+              to: "docs/doc1"
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2"
+            }
+          ]
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus"
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus"
+            }
+          ]
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Blog",
+              to: "blog"
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus"
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus"
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Sapodilla Tech.`
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/facebook/docusaurus/edit/master/website/"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
+};
